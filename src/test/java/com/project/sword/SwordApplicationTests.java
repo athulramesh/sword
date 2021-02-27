9f866778caffb4207c2f8d45b88c5d2fc4a3cd6c
Athul <athumon666@gmail.com>
2021-02-27 14:37:37 +0530

    project framework added

---
+++ src/test/java/com/project/sword/SwordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
